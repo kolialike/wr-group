c1b8641f12ad9298a6a5b3e9a772b516026aeadc
kolialike <kolialike@gmail.com>
2016-06-07 16:59:57 +0300

    commit#2

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 jQuery(function($){
 
-<<<<<<< HEAD
+
   // tabs
   var tabs = $(".tabs").find("a");
   var tabsContent = $(".tabs-content").find(".tabs-container");
@@ -54,22 +54,4 @@ jQuery(function($){
     event.stopPropagation();
   });
 
-=======
-  // var body = $("body");
-  //   //menu
-  // var menuBurger = $(".menu-burger");
-  // menuBurger.on('click', function(event) {
-  // 	event.preventDefault();
-  // 	body.toggleClass('mobile-menu-open');
-  // });
-  // var mobileMenu = $(".mobile-menu");
-  // mobileMenu.on('click', function(event) {
-  // 	event.preventDefault();
-  // 	body.removeClass("mobile-menu-open");
-  // });
-  // var mobileMenuItem = $(".mobile-menu-item");
-  // mobileMenuItem.on('click', function(event) {
-  // 	event.stopPropagation();
-  // });
->>>>>>> 9aecd9abb761d04e6f0b4c141f974974fa92db7c
 });
